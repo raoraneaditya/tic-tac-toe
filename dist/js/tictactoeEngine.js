67c86d913c1a65a51cbe461fd3bb918810aec053
Aditya Raorane <adityaraorane135@gmail.com>
2024-09-22 21:21:17 +0530

    logic pushed

--- dist/js/tictactoeEngine.js
+++ dist/js/tictactoeEngine.js
@@ -1,11 +1,38 @@
 const tttTracker = [];
+const track = 9;
+let xPlayer = 1;
+let oPlayer = 0;
 
+
+
+
+
+// console.log(varItem);
+
+// varItem[i].id
 
 for(let i = 0; i<9; i++){
-    let id = "item-" + (i+1);
-    
-    document.getElementById(id).addEventListener("click", function(){
-        document.getElementById(id).style.background = "#00ff00";
-    });
+    let varItem = document.getElementsByClassName(".item");
+    // let eachItem = document.getElementById("item-" + (i));
+    varItem.addEventListener("click", function(){
+        if (xPlayer == 1){
+            eachItem.style.background = "#00ff00";
+            tttTracker.push({i: "x"});
+            xPlayer = 0;
+            oPlayer = 1;
+        
+        } else if(oPlayer == 1)
+        {
+        
+            eachItem.style.background = "#ff0000";
+            tttTracker.push({i: "o"});
+            oPlayer = 0;
+            xPlayer = 1;
+
+        }
+    }); 
 }
+// if(tttTracker.length == 8){
+    // console.log(tttTracker);
+// }
 
